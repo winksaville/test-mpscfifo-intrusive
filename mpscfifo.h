a3bcf815d6e92a7c99f0906611a55bea2ba76555
Wink Saville <wink@saville.com>
2016-08-10 15:23:36 -0700

    MpscFifo_t.pTail does not need to be _Atomic.
    This makes the rmv code a little smaller a couple fewer xchg instructoins
    and maybe 1ns to 2ns faster as I've seen ns_per_msg=43.0:
    
    ```
    $ time ./test 12 10000000 10000000
    test client_count=12 loops=10000000 msg_count=10000000
         1 7fb0dacb6700  multi_thread_msg:+client_count=12 loops=10000000 msg_count=10000000
         2 7fb0dacb6700  multi_thread_msg: msgs_processed=993372658 msgs_sent=83359236 no_msgs_count=36640764
         3 7fb0dacb6700  startup=4.003963
         4 7fb0dacb6700  looping=31.841492
         5 7fb0dacb6700  disconneting=8.870432
         6 7fb0dacb6700  stopping=0.019009
         7 7fb0dacb6700  complete=1.966396
         8 7fb0dacb6700  processing=42.697s
         9 7fb0dacb6700  msgs_per_sec=23265452
        10 7fb0dacb6700  ns_per_msg=43.0ns
        11 7fb0dacb6700  total=46.701
        12 7fb0dacb6700  multi_thread_msg:-error=0
    
    Success
    
    real	0m46.703s
    user	7m50.340s
    sys	0m22.193s
    ```

--- mpscfifo.h
+++ mpscfifo.h
@@ -52,7 +52,7 @@ typedef struct Msg_t {
 typedef struct MpscFifo_t {
 #if USE_ATOMIC_TYPES
   _Atomic(Msg_t*) VOLATILE pHead __attribute__(( aligned (64) ));
-  _Atomic(Msg_t*) pTail __attribute__(( aligned (64) ));
+  Msg_t* pTail __attribute__(( aligned (64) ));
 #else
   Msg_t* pHead __attribute__(( aligned (64) ));
   Msg_t* pTail __attribute__(( aligned (64) ));
